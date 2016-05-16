81fe6bc0766d44dd8d69b1a8fc67756bcbb356ab
Alexander Voronin <voronin@mercurydevelopment.com>
2016-05-16 13:09:48 +0400

    SHOTSIOS-7010 - General: Prepare for IPv4 ban by Apple. Added dynamic using ipv4 or ipv6 in reachabilty manager dependig on OS version

--- AFNetworking/AFNetworkReachabilityManager.m
+++ AFNetworking/AFNetworkReachabilityManager.m
@@ -144,18 +144,37 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
 
 + (instancetype)manager
 {
-#if (defined(__IPHONE_OS_VERSION_MIN_REQUIRED) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 90000) || (defined(__MAC_OS_X_VERSION_MIN_REQUIRED) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101100)
-    struct sockaddr_in6 address;
-    bzero(&address, sizeof(address));
-    address.sin6_len = sizeof(address);
-    address.sin6_family = AF_INET6;
-#else
-    struct sockaddr_in address;
-    bzero(&address, sizeof(address));
-    address.sin_len = sizeof(address);
-    address.sin_family = AF_INET;
-#endif
-    return [self managerForAddress:&address];
+    typedef union
+    {
+        struct sockaddr_in6 ipv6;
+        struct sockaddr_in  ipv4;
+        struct sockaddr     base;
+    } USocketAddress;
+    
+    static AFNetworkReachabilityManager *_sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        USocketAddress address = {0};
+        
+        NSOperatingSystemVersion operatingSytemVersion = [[NSProcessInfo processInfo] operatingSystemVersion];
+        if (operatingSytemVersion.majorVersion >= 9) {
+            struct sockaddr_in6 ipv6_address;
+            bzero(&ipv6_address, sizeof(ipv6_address));
+            ipv6_address.sin6_len = sizeof(ipv6_address);
+            ipv6_address.sin6_family = AF_INET6;
+            address.ipv6 = ipv6_address;
+        } else {
+            struct sockaddr_in ipv4_address;
+            bzero(&ipv4_address, sizeof(ipv4_address));
+            ipv4_address.sin_len = sizeof(ipv4_address);
+            ipv4_address.sin_family = AF_INET;
+            address.ipv4 = ipv4_address;
+        }
+        
+        _sharedManager = [self managerForAddress:&address.base];
+    });
+    
+    return _sharedManager;
 }
 
 - (instancetype)initWithReachability:(SCNetworkReachabilityRef)reachability {
